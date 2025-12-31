ff8cd64bb91c4c1433c100ab103c354fc566a75b
Ian <emilianoian72@gmail.com>
2025-12-31 00:24:29 -0600

    Se vieron los temas de como abordar Funciones, Enums, Never, Objetos, Tuplas,  Arreglos

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,57 @@ let mensaje = "Hola, mundo!";
 mensaje = "¡Hola, TypeScript!";
 mensaje = "adios";
 console.log(mensaje);
+console.log(typeof []);
+let extinsionDinosaurio = 65500000;
+let dinosaurioFavorito = "Tyrannosaurus Rex";
+let extintos = true;
+function chanchitofelix(config) {
+    return config;
+}
+let animales = ["perro", "feliz,", "felipe"];
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let checks = [];
+let nums2 = [];
+let tupla = [1, "hola"];
+var Talla;
+(function (Talla) {
+    Talla["Chica"] = "s";
+    Talla["Mediana"] = "m";
+    Talla["Grande"] = "l";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+const variable1 = Talla.Grande;
+console.log(variable1);
+var LoadingState;
+(function (LoadingState) {
+    LoadingState[LoadingState["Idle"] = 0] = "Idle";
+    LoadingState[LoadingState["Loading"] = 1] = "Loading";
+    LoadingState[LoadingState["Success"] = 2] = "Success";
+    LoadingState[LoadingState["Error"] = 3] = "Error";
+})(LoadingState || (LoadingState = {}));
+const estado = LoadingState.Success;
+const objeto = { id: 1,
+    nombre: ' OLA',
+    talla: Talla.Mediana,
+    direccion: {
+        numero: 1,
+        calle: "buena vista",
+        pais: "México"
+    }
+};
+const arr = [];
+const fn = (edad) => {
+    if (edad > 17)
+        return 'puedes pasar';
+    return 'no puedes pasar';
+};
+function validadEdad(edad, msg) {
+    if (edad > 17)
+        return 'puedes pasar' + msg;
+    return 'no puedes pasar';
+}
+validadEdad(18, '!!!');
+function ErrorUsuario() {
+    throw new Error('Error de usuario');
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,91 @@ mensaje = "¡Hola, TypeScript!"
 
 mensaje = "adios"
 console.log(mensaje)
+console.log(typeof[])
+
+let extinsionDinosaurio = 65_500_000
+let dinosaurioFavorito = "Tyrannosaurus Rex"
+let extintos = true 
+
+function chanchitofelix (config: any){
+    return config
+}
+
+let animales = ["perro","feliz,", "felipe"]
+let nums: number[] = [1,2,3,4,5,6,7,8,9]
+let checks: boolean[] = []
+let nums2: Array<number> = []
+
+// animales.map(x => x.) el autocompletado sugiere metodos del tipo de dato
+let tupla:[number, string]= [1, "hola"]
+
+enum Talla {Chica = 's', Mediana = 'm', Grande = 'l', ExtraGrande = 'xl'}
+const variable1 = Talla.Grande
+console.log(variable1)
+
+const enum  LoadingState {Idle, Loading, Success, Error}
+
+const estado = LoadingState.Success
+
+
+
+// OBJETOS
+type Direccion = {
+numero: number,
+calle: string,
+pais: string,
+
+
+}
+
+
+
+type Persona = {
+    id: number,
+    nombre: string,
+    talla: Talla,
+    direccion: Direccion,
+}
+const objeto: Persona = 
+{id: 1, 
+    nombre: ' OLA', 
+    talla: Talla.Mediana,
+    direccion:  {
+        numero: 1,
+        calle: "buena vista",
+        pais: "México"
+    }
+}
+
+const arr:Persona[] = []
+
+
+
+//funciones
+const fn: (a: number)=> string = (edad: number) =>{
+
+    if (edad > 17)
+        return 'puedes pasar'
+    return 'no puedes pasar'
+}
+
+function validadEdad (edad: number, msg: string) {
+
+    if (edad > 17)
+        return 'puedes pasar' + msg
+
+    return 'no puedes pasar'
+
+}
+
+
+validadEdad(18, '!!!')
+
+
+
+// Never
+function ErrorUsuario (): never{
+    throw new Error('Error de usuario')
+
+
+}
